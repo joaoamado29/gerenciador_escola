1bc96fab5b5714ac4d0cee7099b4e630a992a011
joaoamado29 <joaoamadolima29@gmail.com>
2026-05-11 22:55:48 -0300

    Nav prof e admin + Reorganizacao pages

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 import streamlit as st
-from pages.navigation import nav_render
+from pages.navigation import nav_aluno, nav_professor, nav_admin
 from ui.styles import load_css
 
 st.set_page_config(initial_sidebar_state="collapsed")
 load_css()
 
 def main() -> None:
-    nav_render()
+    nav_admin()
 
 if __name__ == "__main__":
     main()
